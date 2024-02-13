df7067dbd78d355a098e6fbf33b41d9f9082e379
Maksym Tsapenko <maksym@Maksyms-MacBook-Pro.local>
2024-02-13 16:54:05 +0200

    fix gitignore

--- charity/submissions/requirements.py
+++ charity/submissions/requirements.py
@@ -1,3 +1,2 @@
 def submission_can_be_edited(submission, user):
-    #return submission.is_draft and submission.author == user
-    return True
+    return submission.is_draft and submission.author == user
