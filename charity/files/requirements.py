6051c443d4e213c79a1e677e12e294b45e42bf9c
Maksym Tsapenko <maksym@Maksyms-MacBook-Pro.local>
2024-02-18 16:21:38 +0200

    refactor

--- charity/files/requirements.py
+++ charity/files/requirements.py
@@ -3,34 +3,37 @@ import importlib
 imports = {}
 
 
-def file_is_ready_to_be_removed(file, target, content_type):
+def get_module(content_type):
     try:
         key = f'{content_type.app_label}.requirements'
         if not key in imports.keys():
             imports[key] = importlib.import_module(key)
-
-        module = imports[key]
-        func_name = f'{content_type.model}_file_is_ready_to_be_removed'
-        if hasattr(module, func_name):
-            func = getattr(module, func_name)
-            return func(target, file)
-        else:
-            return True
+        return imports[key]
     except:
+        return None
+
+
+def file_is_ready_to_be_removed(file, target, content_type):
+    module = get_module(content_type)
+    if not module:
         return True
 
-def file_is_ready_to_be_added(target, content_type):
-    try:
-        key = f'{content_type.app_label}.requirements'
-        if not key in imports.keys():
-            imports[key] = importlib.import_module(key)
+    func_name = f'{content_type.model}_file_is_ready_to_be_removed'
+    if hasattr(module, func_name):
+        func = getattr(module, func_name)
+        return func(target, file)
+    else:
+        return True
 
-        module = imports[key]
-        func_name = f'{content_type.model}_file_is_ready_to_be_added'
-        if hasattr(module, func_name):
-            func = getattr(module, func_name)
-            return func(target)
-        else:
-            return True
-    except:
+
+def file_is_ready_to_be_added(target, content_type):
+    module = get_module(content_type)
+    if not module:
+        return True
+    
+    func_name = f'{content_type.model}_file_is_ready_to_be_added'
+    if hasattr(module, func_name):
+        func = getattr(module, func_name)
+        return func(target)
+    else:
         return True
